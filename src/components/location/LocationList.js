3f101264675f6a67cafcdc2abb59d6243c3595d0
Grady Robbins <gradyrobbins13@gmail.com>
2019-01-17 15:40:44 -0600

    a little progress, code-along in class

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+export default class LocationList  extends Component {
+    render() {
+        return (
+            <article>
+                <h1>Location List</h1>
+                <section>North</section>
+                <h3>Student Kennels</h3>
+                <h4>Nashville North Location</h4>
+                <h5>500 Puppy Way</h5>
+                <br/>
+                <section>South</section>
+            </article>
+        );
+    }
+}
