f5a7919d9ab907c51f68ba92caf0fe01aaba2d30
Grady Robbins <gradyrobbins13@gmail.com>
2019-07-16 16:20:35 -0500

    bg-dark obscures overlapping elements

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -2,32 +2,29 @@ import React, { Component } from "react"
 import { Link } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 
-
 class NavBar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-light fixed-top light-blue flex-md-nowrap p-0 shadow">
-                <ul className="nav nav-pills">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">Locations</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/animals">Animals</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/employees">Employees</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/owners">Owners</Link>
-                    </li>
-                    {/* <li className="nav-item">
-                        <Link className="nav-link" to="/friends">Friends</Link>
-                    </li> */}
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/usa">USA</Link>
-                    </li>
-                </ul>
-            </nav>
+            <nav className="navbar navbar-light sticky-top scrolling-navbar text-white bg-dark flex-md-nowrap p-0 shadow" >
+
+                    <ul className="nav nav-pills ">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Locations</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/animals">Animals</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/employees">Employees</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/owners">Owners</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/usa">USA</Link>
+                        </li>
+                    </ul>
+                </nav>
         )
     }
 }
